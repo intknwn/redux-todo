79662bfebea4930cfd1a5b0781764c42c8d9184c
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-08-09 16:46:20 +0300

    Переносит логику todos в todosSlice

--- src/reducer.js
+++ src/reducer.js
@@ -1,46 +1,12 @@
 const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Learn Redux Toolkit', completed: false, color: 'blue' },
-  ],
   filters: {
     status: 'All',
     colors: [],
   },
 }
 
-const nextTodoId = (todos) =>
-  todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1
-
 export default function appReducer(state = initialState, action) {
   switch (action.type) {
-    case 'todos/todoAdded':
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false,
-          },
-        ],
-      }
-    case 'todos/todoToggled':
-      return {
-        ...state,
-        todos: state.todos.map((todo) => {
-          if (todo.id !== action.payload) {
-            return todo
-          }
-
-          return {
-            ...todo,
-            completed: !todo.completed,
-          }
-        }),
-      }
     case 'filters/statusFilterChanged':
       return {
         ...state,
