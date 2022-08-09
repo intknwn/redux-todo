937f2ef75ff91051efe78d97f4c0da66d1ef53b0
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-08-09 17:21:30 +0300

    Добавляет actions для todos

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -29,6 +29,26 @@ export default function todosReducer(state = initialState, action) {
           completed: !todo.completed,
         }
       })
+    case 'todos/colorSelected': {
+      const { id, color } = action.payload
+
+      return state.map((todo) => {
+        if (todo.id !== id) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          color,
+        }
+      })
+    }
+    case 'todos/todoDeleted':
+      return state.filter((todo) => todo.id !== action.payload)
+    case 'todos/allCompleted':
+      return state.map((todo) => ({ ...todo, completed: true }))
+    case 'todos/completedCleared':
+      return state.filter((todo) => !todo.completed)
     default:
       return state
   }
