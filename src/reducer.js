bf492f6b7b979bf01c116d6756dbd0053d013358
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-08-09 16:50:44 +0300

    Переносит логику filters в filtersSlice

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,7 @@
-const initialState = {
-  filters: {
-    status: 'All',
-    colors: [],
-  },
-}
+const initialState = {}
 
 export default function appReducer(state = initialState, action) {
   switch (action.type) {
-    case 'filters/statusFilterChanged':
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      }
     default:
       return state
   }
